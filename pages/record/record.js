20d35b1a56aed350bcbd6198cc50ebb569e45724
BkonKing <1810972990@qq.com>
2019-05-29 23:53:32 +0800

    初步搭建

---
+++ pages/record/record.js
@@ -0,0 +1,82 @@
+// pages/record/record.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    albumList: [
+      {
+        art: '',
+        title: '课堂经典案例',
+        date: '2019/04/22'
+      },
+      {
+        art: '',
+        title: '课堂经典案例1',
+        date: '2019/05/12'
+      },
+      {
+        art: '',
+        title: '课堂经典案例2',
+        date: '2019/02/26'
+      }
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
