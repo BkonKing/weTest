b1b60d61ca84a07206b588fa0cf464c6f60a2a7f
BkonKing <1810972990@qq.com>
2019-05-31 01:06:23 +0800

    报名

--- pages/apply/apply.js
+++ pages/apply/apply.js
@@ -1,15 +1,43 @@
 // pages/apply/apply.js
+var util = require("../../utils/util.js");
+var app = getApp();
 var sliderWidth = 96; // 需要设置slider的宽度，用于计算中间位置
 
 Page({
   data: {
-    tabs: ["选项一", "选项二", "选项三"],
+    tabs: ["报名学生", "往届学生", "报名"],
     activeIndex: 0,
     sliderOffset: 0,
-    sliderLeft: 0
+    sliderLeft: 0,
+    studentsList: [
+      {
+        id: 'sdf',
+        name: '李寻欢',
+        age: 11,
+        phone: 13612345621,
+        situation: 1
+      }
+    ],
+    previousList: [
+      {
+        id: 'sdf',
+        name: '小飞刀',
+        age: 18,
+        phone: 179213458,
+        situation: 1
+      }
+    ],
+    show: false,
+    applyDate: '',
+    deadlineDate: '',
+    today: '',
+    IMG_URL: app.globalData.imgurl
   },
   onLoad: function () {
     var that = this;
+    this.setData({
+      today: util.formatDate(new Date())
+    })
     wx.getSystemInfo({
       success: function (res) {
         that.setData({
@@ -24,5 +52,26 @@ Page({
       sliderOffset: e.currentTarget.offsetLeft,
       activeIndex: e.currentTarget.id
     });
+  },
+  getPersonal: function (e) {
+    this.setData({
+      show: true
+    })
+  },
+  bindApplyDateChange: function (e) {
+    this.setData({
+      applyDate: e.detail.value
+    })
+  },
+  bindDeadlineDate: function (e) {
+    console.log(e.detail.value)
+    this.setData({
+      deadlineDate: e.detail.value
+    })
+  },
+  callUp: function () {
+    wx.makePhoneCall({
+      phoneNumber: '15505919792'
+    })
   }
 });
